8ca2aa5e65b27eebae4cb3e8958b45af476da835
woodrow-snow <woodrow2021@outlook.com>
2026-01-21 13:27:20 -0700

    W03 Learning Activity 3/3

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require('./controllers/baseController');
 const inventoryRoute = require('./routes/inventoryRoute.js');
+const utilities = require('./utilities/');
 
 
 /* ***********************************************
@@ -29,10 +30,30 @@ app.set("layout", "./layouts/layout"); // not at views roots
 app.use(static)
 
 // index route
-app.get("/", baseController.buildHome);
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
 // Inventory routes
 app.use("/inv", inventoryRoute);
+
+// file not found Route  must be last route in list
+app.use(async (req, res, next) => {
+  next({ status: 404, message: 'This is not the page you are looking for.'})
+});
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => { 
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
+  if (err.status == 404) { mesage = err.message } else { message = 'Oh no! There was a crash. Maybe try a different route?' };
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  });
+});
 
 /* ***********************
  * Local Server Information
--- utilities/index.js
+++ utilities/index.js
@@ -52,6 +52,12 @@ Util.buildClassificationGrid = async function (data) {
     return grid;
 }
 
+/* ****************************************
+ * Middleware For Handling Errors
+ * Wrap other function in this for 
+ * General Error Handling
+ **************************************** */
+Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
 
 
 
